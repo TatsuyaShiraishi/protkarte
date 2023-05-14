25437a666ad04fa639581650389a32379428b9f9
TatsuyaShiraishi <48981610+TatsuyaShiraishi@users.noreply.github.com>
2023-05-14 20:25:38 +0900

    Update saveText.js

--- saveText.js
+++ saveText.js
@@ -1,16 +1,32 @@
-module.exports = (req, res) => {
+const { MongoClient } = require('mongodb');
+
+module.exports = async (req, res) => {
   if (req.method === 'POST') {
-    // リクエストボディからテキストデータを取得
     const textData = req.body.text;
 
-    // テキストデータをデータベースに保存するロジックを実装する
-    // 例: MongoDBを使用する場合
-    // const savedData = saveToMongoDB(textData);
+    try {
+      // MongoDBへの接続
+      const uri = 'mongodb://<ホスト名>:<ポート番号>/<データベース名>';
+      const client = new MongoClient(uri);
+      await client.connect();
 
-    // 必要に応じてレスポンスを返す
-    res.status(200).json({ message: 'テキストが保存されました。' });
+      // コレクションの取得
+      const database = client.db('<データベース名>');
+      const collection = database.collection('<コレクション名>');
+
+      // テキストデータの保存
+      const result = await collection.insertOne({ text: textData });
+
+      // 接続のクローズ
+      await client.close();
+
+      // レスポンスを返す
+      res.status(200).json({ message: 'テキストが保存されました。', insertedId: result.insertedId });
+    } catch (error) {
+      console.error('データベースエラー:', error);
+      res.status(500).json({ error: 'データベースエラーが発生しました。' });
+    }
   } else {
-    // POST以外のリクエストにはエラーレスポンスを返す
     res.status(405).json({ error: 'メソッドは許可されていません。' });
   }
 };
