8017cb5283a4fa4058b5e98aada0345f9f1a3bf1
TatsuyaShiraishi <48981610+TatsuyaShiraishi@users.noreply.github.com>
2023-05-14 22:46:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+// Firebaseの初期化
+firebase.initializeApp({
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  // その他の設定...
+});
+
+// Firestoreへの参照を取得
+const firestore = firebase.firestore();
+
 // 保存ボタンをクリックしたときの処理
-document.getElementById('saveButton').addEventListener('click', function() {
-  // 入力されたテキストを取得
-  var inputText = document.getElementById('inputArea').value;
+document.getElementById('saveButton').addEventListener('click', async function() {
+  const inputText = document.getElementById('inputArea').value;
 
-  // 現在の日時を取得
-  var currentDate = new Date();
-  var timestamp = currentDate.toLocaleString();
+  try {
+    // テキストデータを保存
+    await firestore.collection('texts').add({ text: inputText });
+    
+    // 保存成功メッセージを表示
+    const savedTextElement = document.getElementById('savedText');
+    savedTextElement.textContent = 'テキストが保存されました。';
 
-  // テキストと日時を表示
-  var savedTextElement = document.getElementById('savedText');
-  savedTextElement.textContent = '保存されたテキスト: ' + inputText + ' (保存日時: ' + timestamp + ')';
+    // 入力エリアをクリア
+    document.getElementById('inputArea').value = '';
+  } catch (error) {
+    console.error('データベースエラー:', error);
+    // エラーメッセージを表示
+    const savedTextElement = document.getElementById('savedText');
+    savedTextElement.textContent = '保存中にエラーが発生しました。';
+  }
 });
